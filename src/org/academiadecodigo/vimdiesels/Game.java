e6d3aa976561801f3ae9b53baf8501a507176336
Tiago Rafael Mina Azevedo <tiagomina.azevedo@gmail.com>
2019-10-10 19:35:24 +0100

    Playable Stage work

--- src/org/academiadecodigo/vimdiesels/Game.java
+++ src/org/academiadecodigo/vimdiesels/Game.java
@@ -2,20 +2,21 @@ package org.academiadecodigo.vimdiesels;
 
 import org.academiadecodigo.vimdiesels.grid.Grid;
 import org.academiadecodigo.vimdiesels.grid.GridFactory;
+import org.academiadecodigo.vimdiesels.grid.PlayableStage;
 
 public class Game {
 
     private Grid grid;
-    private Grid playableGrid;
+    private PlayableStage playableStage;
 
     public Game() {
         this.grid = GridFactory.makeGrid(29,25);
-        this.playableGrid = GridFactory.makeGrid(25,21);
+        this.playableStage = new PlayableStage();
     }
 
     public void init(){
         grid.init();
-        playableGrid.init();
+        playableStage.init();
 
     }
 }
