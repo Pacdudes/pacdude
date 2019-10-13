3a9b88fba16536474bf33d6e731018f6558aba3e
Danones <diogo.sousa.uk@gmail.com>
2019-10-13 17:10:31 +0100

    added the background image

--- src/org/academiadecodigo/vimdiesels/gfx/MainMenu.java
+++ src/org/academiadecodigo/vimdiesels/gfx/MainMenu.java
@@ -20,11 +20,13 @@ public class MainMenu {
 
         menuBackground = new Picture(0,0, pathImages + "mainmenuBackground.png");
 
+        menuBackground.draw();
        // menuOptions[0] = new Picture(336,700, MenuOptions.START_GAME.getUnselected());
         menuSelected[0] = new Picture(336,700,MenuOptions.START_GAME.getSelected());
-
-        menuOptions[1] = new Picture()
-
-
+        menuSelected[0].draw();
+        //        menuOptions[1] = new Picture()
+        menuSelected[1] = new Picture(336,650, MenuOptions.QUIT.getSelected());
+        menuSelected[0].draw();
     }
+
 }
